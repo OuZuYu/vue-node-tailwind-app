3df5e32fa643de4151db18a9930cd1ab88ff1d81
ozy <oupenguin@gmail.com>
2023-03-25 19:40:42 +0800

    posts 接口 try catch 处理

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -3,19 +3,37 @@ const router = express.Router()
 const { Posts } = require('../models')
 
 router.get('/', async (req, res) => {
-  const list = await Posts.findAll()
-  res.json(list)
+  try {
+    const list = await Posts.findAll()
+    res.json(list)
+  } catch (err) {
+    res.status(500).json({
+      error: err
+    })
+  }
 })
 
 router.post('/', async (req, res) => {
-  const post = req.body
-  await Posts.create(post)
-  res.json(post)
+  try {
+    const post = req.body
+    await Posts.create(post)
+    res.json(post)
+  } catch (err) {
+    res.status(500).json({
+      error: err
+    })
+  }
 })
 
 router.get('/:id', async (req, res) => {
-  const post = await Posts.findByPk(req.params.id)
-  res.json(post)
+  try {
+    const post = await Posts.findByPk(req.params.id)
+    res.json(post)
+  } catch (err) {
+    res.status(500).json({
+      error: err
+    })
+  }
 })
 
 module.exports = router
